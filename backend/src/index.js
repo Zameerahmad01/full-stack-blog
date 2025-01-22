b3055052c44df44afc80cd2c2586d9d91b28ffa1
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-01-22 22:48:45 +0500

    Implement initial backend server setup: create index.js to configure Express server, set up middleware for JSON and URL-encoded data, and establish API routes for users, posts, and comments. Connect to MongoDB and start the server, enhancing the backend functionality.

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import { ConnectDB } from "./db/index.js";
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+
+//routes
+import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
+import commentRoutes from "./routes/comment.routes.js";
+app.use("/api/users", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+
+//connect database and run server
+ConnectDB()
+  .then((res) => {
+    console.log("connect to database mongodb");
+    app.listen(port, () => {
+      console.log(`server is listening on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection error", err);
+  });
