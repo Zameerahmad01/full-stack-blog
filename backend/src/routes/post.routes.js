8ef2a317f693609eac7332879611e7f421f0ae4b
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-01-27 22:30:34 +0500

    Add authentication middleware to post routes

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -5,12 +5,13 @@ import {
   createPost,
   deletePost,
 } from "../controllers/post.controller.js";
+import { verifyAuth } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get("/", getPosts);
 router.get("/:slug", getPost);
-router.post("/", createPost);
-router.delete("/:id", deletePost);
+router.post("/", verifyAuth, createPost);
+router.delete("/:id", verifyAuth, deletePost);
 
 export default router;
