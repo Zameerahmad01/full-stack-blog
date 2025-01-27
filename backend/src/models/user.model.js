13903dba81bc3b551b40d30c9a92f90e7b2c7efd
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-01-27 22:29:06 +0500

    Add clerkId field to user model for authentication

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,6 +2,11 @@ import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
   {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     userName: {
       type: String,
       required: true,
