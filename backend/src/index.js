be419773dd9fbcba1641641cd4ac9e245efbd762
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-01-27 22:31:15 +0500

    Configure Clerk authentication middleware and CORS settings in Express app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import { ConnectDB } from "./db/index.js";
+import webhooksRoutes from "./routes/webhooks.routes.js";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(clerkMiddleware());
+app.use("/api/webhooks", webhooksRoutes);
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.json({ limit: "16kb" }));
