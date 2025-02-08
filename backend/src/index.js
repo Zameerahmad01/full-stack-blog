ebacbbc9e45e01cbd8ae054bd7eb8202a91f24a9
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-02-08 23:32:08 +0500

    Add CORS middleware to enable cross-origin requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,6 +23,15 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 //routes
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
