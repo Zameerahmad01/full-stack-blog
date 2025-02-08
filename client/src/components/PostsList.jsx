ebe6e406d143470c0010fefe81db033b27782ac6
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-02-08 23:34:39 +0500

    Implement posts fetching with React Query in PostsList component

--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -1,6 +1,28 @@
 import PostItem from "./PostItem";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchPosts = async () => {
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`);
+  return res.data;
+};
 
 const PostsList = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => fetchPosts(),
+  });
+
+  if (isPending) {
+    return <span> Loading...</span>;
+  }
+
+  if (error) {
+    return "An error has occurred: " + error.message;
+  }
+
+  console.log(data);
+
   return (
     <div className="flex flex-col gap-8 mb-8 ">
       <PostItem />
