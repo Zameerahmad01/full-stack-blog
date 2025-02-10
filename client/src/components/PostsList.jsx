93e8bb84e58b1a3f5cae0f381c56b185e4caed82
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-02-10 21:36:09 +0500

    Implement infinite scroll for PostsList with dynamic post loading

--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -1,39 +1,63 @@
 import PostItem from "./PostItem";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-const fetchPosts = async () => {
-  const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`);
+const fetchPosts = async (pageParams) => {
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`, {
+    params: {
+      page: pageParams,
+      limit: 2,
+    },
+  });
   return res.data;
 };
 
 const PostsList = () => {
-  const { isPending, error, data } = useQuery({
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
     queryKey: ["posts"],
-    queryFn: () => fetchPosts(),
+    queryFn: ({ pageParam = 1 }) => fetchPosts(pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) =>
+      lastPage.hasMore ? pages.length + 1 : undefined,
   });
 
-  if (isPending) {
+  const allPosts = data?.pages.flatMap((page) => page.posts) || [];
+
+  if (status === "pending") {
     return <span> Loading...</span>;
   }
 
-  if (error) {
-    return "An error has occurred: " + error.message;
+  if (status === "error") {
+    return "something went wrong";
   }
 
   console.log(data);
 
   return (
-    <div className="flex flex-col gap-8 mb-8 ">
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-    </div>
+    <InfiniteScroll
+      dataLength={allPosts.length}
+      next={fetchNextPage}
+      hasMore={!!hasNextPage}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p>
+          <b>All posts are loaded!</b>
+        </p>
+      }
+    >
+      {allPosts.map((post) => (
+        <PostItem key={post._id} post={post} />
+      ))}
+    </InfiniteScroll>
   );
 };
 
