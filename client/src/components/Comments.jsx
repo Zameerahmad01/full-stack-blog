d10cbc34d60e9ca96201f60002d286facfee03a5
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-02-10 21:37:09 +0500

    Implement dynamic Comments component with React Query and comment submission

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,24 +1,104 @@
+import axios from "axios";
 import Comment from "./Comment";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { toast } from "react-toastify";
+import { useState } from "react";
 
-const Comments = () => {
+const fetchComments = async (postId) => {
+  const response = await axios.get(
+    `${import.meta.env.VITE_API_URL}/comment/${postId}`
+  );
+  return response.data;
+};
+
+const Comments = ({ postId }) => {
+  const { getToken } = useAuth();
+  const { user } = useUser();
+
+  const { data, error, isPending } = useQuery({
+    queryKey: ["comments", postId],
+    queryFn: () => fetchComments(postId),
+  });
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: async ({ postId, newComment }) => {
+      const token = await getToken();
+      return axios.post(
+        `${import.meta.env.VITE_API_URL}/comment/${postId}`,
+        newComment,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["comments", postId]);
+    },
+    onError: () => {
+      toast.error("Something went wrong...");
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const data = {
+      description: formData.get("description"),
+    };
+    mutation.mutate({ postId, newComment: data });
+  };
+
+  // console.log(data);
+
   return (
     <div className="mt-8 flex flex-col gap-8 w-3/5">
       <h1 className="underline text-gray-500 font-medium text-xl">Comments</h1>
-      <div className="flex items-center justify-between gap-8 w-full">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center justify-between gap-8 w-full"
+      >
         <textarea
           placeholder="Write a comment..."
           className="w-full p-4 rounded-xl"
+          name="description"
         ></textarea>
-        <button className="px-6 py-4 bg-blue-800 font-medium text-white rounded-xl">
+        <button
+          type="submit"
+          className="px-6 py-4 bg-blue-800 font-medium text-white rounded-xl"
+        >
           Send
         </button>
-      </div>
+      </form>
       <div className="flex flex-col gap-4">
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
+        {isPending ? (
+          "loading..."
+        ) : error ? (
+          "Something went wrong..."
+        ) : (
+          <>
+            {mutation.isPending && (
+              <Comment
+                comment={{
+                  description: mutation.variables.newComment.description,
+                  createdAt: "sending...",
+                  user: {
+                    img: user?.imageUrl,
+                    userName: user?.username,
+                  },
+                }}
+              />
+            )}
+
+            {data?.map((comment) => (
+              <Comment key={comment._id} comment={comment} />
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
