2f2f2338cb435286385e51f2d13d2f9002c35701
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-02-10 21:35:57 +0500

    Implement dynamic SinglePostPage with data fetching and SEO metadata

--- client/src/pages/SinglePostPage.jsx
+++ client/src/pages/SinglePostPage.jsx
@@ -1,46 +1,72 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Image from "../components/Image";
 import PostMenuAction from "../components/PostMenuAction";
 import Search from "../components/Search";
 import Comments from "../components/Comments";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { format } from "timeago.js";
+import { Helmet } from "react-helmet";
+
+const fetchPost = async (slug) => {
+  const response = await axios.get(
+    `${import.meta.env.VITE_API_URL}/posts/${slug}`
+  );
+  return response.data;
+};
 
 const SinglePostPage = () => {
+  const { slug } = useParams();
+
+  const { data, error, isPending } = useQuery({
+    queryKey: ["posts", slug],
+    queryFn: () => fetchPost(slug),
+  });
+
+  // console.log(data);
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>something went wrong....</div>;
+  }
+  if (!data) {
+    return <div>Post not found...</div>;
+  }
   return (
-    <section className="flex flex-col gap-8 mt-8">
+    <section className="flex flex-col gap-8 mt-8 py-8">
       {/* details  */}
       <div className="flex gap-8">
         {/* content  */}
         <div className="flex flex-col gap-8 lg:w-3/5">
-          <h1
-            to="/test"
-            className="font-semibold text-xl md:text-3xl xl:text-4xl"
-          >
+          <h1 className="font-semibold text-xl md:text-3xl xl:text-4xl">
             {" "}
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
-            reprehenderit molestiae odit doloribus.
+            {data?.title}{" "}
           </h1>
           <div className="flex items-center gap-2 text-sm">
             <span className="text-sm text-gray-400">Written By</span>
-            <Link className="font-medium text-blue-800">John Doe</Link>
+            <Link className="font-medium text-blue-800">
+              {data?.user.userName}
+            </Link>
             <span className="text-sm text-gray-400">on</span>
-            <Link className="font-medium text-blue-800">Web Design</Link>
-            <span className="text-sm text-gray-400">2 days ago</span>
+            <Link className="font-medium text-blue-800">{data.category}</Link>
+            <span className="text-sm text-gray-400">
+              {format(data?.createdAt)}
+            </span>
           </div>
-          <p className="text-gray-500 font-medium">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. A animi
-            accusamus eius ad deserunt doloremque esse nesciunt nostrum. Ducimus
-            fugiat, dolor excepturi ratione quos commodi nulla perferendis alias
-            quia est.
-          </p>
+          <p className="text-gray-500 font-medium">{data?.description}</p>
         </div>
 
         {/* image  */}
         <div className="hidden lg:block lg:w-2/5">
           <Image
-            src="postImg.jpeg"
+            src={data?.img}
             alt="post image"
-            className="rounded-2xl h-full w-full object-cover"
+            className="rounded-2xl h-full w-full object-center"
             width="600"
+            height="400"
           />
         </div>
       </div>
@@ -49,7 +75,16 @@ const SinglePostPage = () => {
       <div className="flex flex-col md:flex-row gap-12 mt-8">
         {/* text  */}
         <div className="flex flex-col gap-6 text-justify lg:text-lg ">
-          <p>
+          <div dangerouslySetInnerHTML={{ __html: data?.content }}></div>
+          <Helmet>
+            <title>{data?.title}</title>
+            <meta name="description" content={data?.description} />
+            <meta property="og:title" content={data?.title} />
+            <meta property="og:description" content={data?.description} />
+            <meta property="og:image" content={data?.img} />
+            <meta property="og:url" content={window.location.href} />
+          </Helmet>
+          {/* <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
             odit cupiditate, recusandae neque adipisci porro nihil distinctio
             doloremque accusantium aut accusamus amet cumque sint, tempore atque
@@ -113,23 +148,23 @@ const SinglePostPage = () => {
             soluta magnam, autem provident, sint nihil reiciendis mollitia
             sapiente hic officia blanditiis quam possimus excepturi quasi vel
             sequi tenetur.
-          </p>
+          </p> */}
         </div>
         {/* detail  */}
         <div className="px-4 sticky top-8 h-max">
           <h1 className="mb-4 text-sm font-medium">Author</h1>
           <div className="flex flex-col gap-4">
             <div className="flex gap-8 items-center">
-              <Image
-                src="userImg.jpeg"
+              <img
+                src={data?.user.img}
                 alt="user image"
                 className="w-12 h-12 rounded-full object-cover"
                 width="48"
                 height="48"
               />
-              <h2 className="">Jhon Doe</h2>
+              <h2 className="">{data?.user.userName}</h2>
             </div>
-            <p className="text-sm">Lorem ipsum dolor sit amet consectetur.</p>
+            <p className="text-sm">{data?.user.email}</p>
             <div className="flex items-center gap-2">
               <Link>
                 <Image
@@ -168,7 +203,7 @@ const SinglePostPage = () => {
           </div>
         </div>
       </div>
-      <Comments />
+      <Comments postId={data?._id} />
     </section>
   );
 };
