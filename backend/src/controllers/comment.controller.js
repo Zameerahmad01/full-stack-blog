9c532c9f1a05bb40d03622f68c2521d14a576045
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-02-14 23:30:24 +0500

    Add admin comment deletion functionality with consistent response

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -23,6 +23,12 @@ const createComment = async (req, res) => {
 
 const deleteComment = async (req, res) => {
   const user = req.user;
+  const role = req.auth.sessionClaims.metadata.role || "user";
+
+  if (role === "admin") {
+    await Comment.findOneAndDelete({ _id: req.params.id });
+    return res.status(200).json({ message: "comment has been deleted" });
+  }
   const comment = await Comment.findOneAndDelete({
     _id: req.params.id,
     user: user._id,
@@ -32,6 +38,6 @@ const deleteComment = async (req, res) => {
       .status(403)
       .json({ message: "You are not allowed to delete this comment" });
   }
-  res.status(200).json(comment);
+  return res.status(200).json({ message: "comment has been deleted" });
 };
 export { getComments, createComment, deleteComment };
