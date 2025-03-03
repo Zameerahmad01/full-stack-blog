ccc6aee4b04b2dc1c7c8eb4159b7d4dce36c3f55
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-03-03 12:33:59 +0500

    Apply increaseVisits middleware to single post route

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -8,12 +8,13 @@ import {
   featurePost,
 } from "../controllers/post.controller.js";
 import { verifyAuth } from "../middlewares/auth.middleware.js";
+import increaseVisits from "../middlewares/increaseVisits.js";
 
 const router = Router();
 
 router.get("/upload-auth", uploadAuth);
 router.get("/", getPosts);
-router.get("/:slug", getPost);
+router.get("/:slug", increaseVisits, getPost);
 router.post("/", verifyAuth, createPost);
 router.delete("/:id", verifyAuth, deletePost);
 router.patch("/feature", verifyAuth, featurePost);
